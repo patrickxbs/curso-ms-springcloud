6fdf5bc159895c4f549a94a1b66ff2171fa5882f
Emanuel Patrick <emanuelpatrick440@gmail.com>
2025-09-27 20:03:27 -0300

    subindo api-gateway

---
+++ api-gateway/src/main/java/com/patrick/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,18 @@
+package com.patrick.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/book/**").uri("lb://book-service"))
+                .route(p -> p.path("/exchange/**").uri("lb://exchange-service"))
+                .build();
+    }
+}
